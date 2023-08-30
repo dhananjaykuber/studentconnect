fa646f8db80feb6d8874e3ea713dab9b44a64049
dhananjaykuber <dnkuber2002@gmail.com>
2023-08-31 00:21:24 +0530

    login file issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Layout from "./components/Layout";
-import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import Blogs from "./pages/Blogs";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blogs" element={<Blogs />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,9 +55,8 @@ const Navbar = () => {
                 ? "url('/studentconnect-logo-dark.svg')"
                 : "url('/studentconnect-logo-light.svg')",
           }}
-        >
-          {/* <img src={Logo} className="h-12" alt="logo" /> */}
-        </Link>
+          onClick={() => setShowMobileNav(false)}
+        ></Link>
         <div className="flex items-center md:order-2">
           {/* <Button
             label={"Login"}
@@ -138,6 +137,7 @@ const Navbar = () => {
                 <Link
                   to={link.url}
                   className="block rounded py-2 pl-3 pr-4 text-sm font-semibold text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                  onClick={() => setShowMobileNav(false)}
                 >
                   {link.label}
                 </Link>
